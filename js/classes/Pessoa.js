e4c85cd99192e885ea70ffc9f54ff8435fd4cda8
Filipi <144926216+LipiLima14@users.noreply.github.com>
2025-03-14 17:01:44 -0300

    Update Pessoa.js conditions

--- js/classes/Pessoa.js
+++ js/classes/Pessoa.js
@@ -4,6 +4,7 @@ export class Pessoa {
     _height
     _weight
     _imc
+    _condition
 
     constructor(name, age, height, weight) {
         this._name = name
@@ -11,5 +12,24 @@ export class Pessoa {
         this._height = height
         this._weight = weight
         this._imc = (this._weight / (this._height * this._height)).toFixed(2)
+        this._condition = this.conditions()
+    }
+
+    conditions() {
+        if (this._imc < 18.5) {
+            return "Abaixo do peso."
+        } else if (this._imc <= 25) {
+            return "Peso ideal."
+        } else if (this._imc <= 30) {
+            return "Acima do peso."
+        } else if (this._imc <= 35) {
+            return "Obesidade I"
+        } else if (this._imc < 40) {
+            return "Obesidade II"
+        } else if (this._imc >= 40) {
+            return "Obesidade III"
+        } else {
+            return "Digite suas informaçoes."
+        }
     }
 }
